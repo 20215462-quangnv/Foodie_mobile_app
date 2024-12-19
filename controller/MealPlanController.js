5d578b46cf3f411248942024a86f5d70f52f29fd
IMnhvjp <levanminh19102003@gmail.com>
2024-12-19 14:39:34 +0700

    add meal-plan api

---
+++ controller/MealPlanController.js
@@ -0,0 +1,93 @@
+const API_URL = 'http://localhost:8080/api/meal';
+
+module.exports = {
+    getMealPlan : async (groupId, date) => {
+        try {
+            const response = await fetch(`${API_URL}?date=${date}&groupId=${groupId}`, {
+                method: 'GET',
+                headers: {
+                  'Content-Type': 'application/json',
+                  'Authorization': `Bearer ${token}`,
+                },
+            })
+            if (response.ok) {
+                console.log('fetch meal-plan thanh cong')
+                return response.json();
+            }
+            else {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+        }
+        catch (error) {
+            console.error('Error fetching meal-plan:', error); 
+            throw error;
+        }
+    },
+    createMealPlan : async (newPlan) => {
+        try {
+            const response = await fetch(`${API_URL}`, {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                  'Authorization': `Bearer ${token}`,
+                },
+                body: JSON.stringify(newPlan)
+            })
+            if (response.ok) {
+                console.log('tao meal-plan thanh cong')
+                return response.json();
+            }
+            else {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+        }
+        catch (error) {
+            console.error('Error creating meal-plan:', error); 
+            throw error;
+        }
+    },
+    deleteMealPlan : async (planId) => {
+        try {
+            const response = await fetch(`${API_URL}?Id=${planId}`, {
+                method: 'DELETE',
+                headers: {
+                  'Content-Type': 'application/json',
+                  'Authorization': `Bearer ${token}`,
+                },
+            })
+            if (response.ok) {
+                console.log('xoa mealPlan thanh cong')
+                return response.json();
+            }
+            else {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+        }
+        catch (error) {
+            console.error('Error deleting meal-plan:', error); 
+            throw error;
+        }
+    },
+    updateMealPlan : async (planId) => {
+        try {
+            const response = await fetch(`${API_URL}?Id=${planId}`, {
+                method: 'PUT',
+                headers: {
+                  'Content-Type': 'application/json',
+                  'Authorization': `Bearer ${token}`,
+                },
+            })
+            if (response.ok) {
+                console.log('update mealPlan thanh cong')
+                return response.json();
+            }
+            else {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+        }
+        catch (error) {
+            console.error('Error updating meal-plan:', error); 
+            throw error;
+        }
+    }
+}
