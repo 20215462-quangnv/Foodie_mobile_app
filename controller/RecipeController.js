39c4497689cff824d1e66c0ab695fd0da5f38168
IMnhvjp <levanminh19102003@gmail.com>
2024-12-30 10:37:32 +0700

    fix url

--- controller/RecipeController.js
+++ controller/RecipeController.js
@@ -1,5 +1,5 @@
 import { getToken } from "../controller/AuthController";
-const API_URL = "http://10.0.2.2:8080/api/recipe";
+const API_URL = "http://192.168.43.107:8080/api/recipe";
 
 // Hàm để lấy Bearer token
 const getBearerAuth = async () => {
